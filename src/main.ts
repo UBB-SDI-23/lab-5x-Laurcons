8171533e30a32f21c8551607f55bf33bb0cc6e78
Laurcons <laurcons@outlook.com>
2023-04-04 21:09:33 +0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const swagger = new DocumentBuilder()
     .setTitle("CTP Manager API")

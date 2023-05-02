3afaa1f26e6e2228d0029cc889b75f95d39491e8
Laurcons <laurcons@outlook.com>
2023-05-02 18:04:00 +0300

    fix: add timeout to email verification

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -3,14 +3,17 @@ import PrismaService from './prisma.service';
 import RegisterDto from '../dto/user/register.dto';
 import { UserStatus } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import * as crypto from 'crypto';
+import * as jwt from 'jsonwebtoken';
+import { config } from 'src/lib/config';
 
 @Injectable()
 export default class UserService {
   constructor(private prisma: PrismaService) {}
 
   async register(data: RegisterDto) {
-    const emailActivationCode = crypto.randomUUID();
+    const emailActivationCode = jwt.sign({}, config.jwtSecret, {
+      expiresIn: '10m',
+    });
     const password = await bcrypt.hash(data.password, 12);
     return this.prisma.user.create({
       data: {
@@ -22,6 +25,7 @@ export default class UserService {
   }
 
   async verifyEmailCode(emailActivationCode: string) {
+    jwt.verify(emailActivationCode, config.jwtSecret);
     const user = await this.prisma.user.update({
       where: { emailActivationCode },
       data: {
